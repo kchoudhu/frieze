2822707cf5c59114783464af5ba90d4d94a34076
kchoudhu <kamil.choudhury@anserinae.net>
2018-11-01 13:15:47 +0000

    Add ability to add site to domain

--- frieze/_core.py
+++ frieze/_core.py
@@ -9,7 +9,10 @@ from openarc.exception import OAGraphRetrieveError
 
 class OAG_FriezeDomain(OAG_RootNode):
     @property
-    def context(self): return "frieze"
+    def is_unique(cls): return True
+
+    @property
+    def context(cls): return "frieze"
 
     @staticproperty
     def dbindices(cls): return {
@@ -21,9 +24,37 @@ class OAG_FriezeDomain(OAG_RootNode):
         'domain' : [ 'text',     str(), None ],
     }
 
+    def add_site(self, sitename):
+        try:
+            site = OAG_FriezeSite((self, sitename), 'by_name')
+        except OAGraphRetrieveError:
+            site =\
+                OAG_FriezeSite().db.create({
+                    'domain'   : self,
+                    'sitename' : sitename,
+                })
+
+        return site
+
+class OAG_FriezeSite(OAG_RootNode):
+    @property
+    def context(cls): return "frieze"
+
+    @staticproperty
+    def dbindices(cls): return {
+        'name' : [ ['domain','sitename'], True, None ],
+    }
+
+    @staticproperty
+    def streams(cls): return {
+        'domain'   : [ OAG_FriezeDomain, None,  None ],
+        'sitename' : [ 'text',           str(), None ]
+    }
+
 ####### Exportable friendly names go here
 
 FriezeDomain = OAG_FriezeDomain
+FriezeSite = OAG_FriezeSite
 
 ####### User api goes here
 
