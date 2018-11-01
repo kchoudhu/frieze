3d7362eeab7acebbe512c2a1e3480639eb8939c0
kchoudhu <kamil.choudhury@anserinae.net>
2018-11-01 11:19:36 +0000

    Add skeleton setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(name='frieze',
+      version='0.1',
+      description='Programmable frontend for Ansible',
+      classifiers=[
+        'Development Status :: 1 - Planning',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: System :: Clustering',
+      ],
+      keywords='ansible clusters configuration',
+      url='http://github.com/kchoudhu/frieze',
+      author='Kamil Choudhury',
+      author_email='kamil.choudhury@anserinae.net',
+      license='BSD',
+      packages=['frieze'],
+      install_requires=[
+        'openarc'
+      ],
+      include_package_data=True,
+      zip_safe=False)
