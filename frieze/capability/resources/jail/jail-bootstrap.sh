4bfe862255a4aaeb461e9235ffffe1e22ceec2bf
kchoudhu <kamil.choudhury@anserinae.net>
2019-03-08 19:03:27 +0000

    Clean up jail-boostrap.sh
    Just because it's shell doesn't mean we can behave like animals.

--- frieze/capability/resources/jail/jail-bootstrap.sh
+++ frieze/capability/resources/jail/jail-bootstrap.sh
@@ -1,136 +1,181 @@
 #!/bin/sh -e
-${"""
+
 # Don't let freebsd-update block on pager
 export PAGER=cat
 
-# Datasets and their mountpoints
 DATE=`date +"%Y%m%d%H%M%S"`
 OS_JAIL=12.0-RELEASE
+
+#### Root datasets
 DS_JAIL=zroot/jails
 MP_JAIL=/usr/local/jails
 
+#### Thinjail layers
+#
+# Layer 1: RELEASE
+#
+# Build area for each supported release
 RELEASE_DS=${DS_JAIL}/release/${OS_JAIL}
-RELEASE_MP=${MP_JAIL}/release/${OS_JAIL}
-RELEASE_SNAP=${RELEASE_DS}@${DATE}
-
-TEMPLATE_DS=${DS_JAIL}/template
-TEMPLATE_MP=${MP_JAIL}/template
-
-BASE_DS=${TEMPLATE_DS}/base/${OS_JAIL}
-BASE_MP=${TEMPLATE_MP}/base/${OS_JAIL}
-
+#
+# Layer 2: BASE
+#
+# Template OS, common to all jails. Update this when you want to create security
+# updates
+BASE_DS=${DS_JAIL}/base/${OS_JAIL}
+#
+# Layer 3: SKELETON
+#
+# Template thinjail components, a snapshot of which mounted into every thinjail.
 SKELETON_DS=${DS_JAIL}/skeleton/${OS_JAIL}
-SKELETON_MP=${MP_JAIL}/skeleton/${OS_JAIL}
-
+#
+# Layer 4: THINJAIL
+#
+# Thin, variable parts of individual jails live here.
 THINJAIL_DS=${DS_JAIL}/thinjail
 THINJAIL_MP=${MP_JAIL}/thinjail
 
-# Progress sentinels
-SENTINEL_DIR=${RELEASE_MP}/sentinel
-SENTINEL_FIRST=${SENTINEL_DIR}/first
-SENTINEL_UPD_FETCH=${SENTINEL_DIR}/upd-fetch
-SENTINEL_REL_DONE=${SENTINEL_DIR}/rel
-
-echo "Creating datasets"
-if [ ! "`zfs list | egrep ${DS_JAIL}`" ]; then
-    echo "  zfs create: ${DS_JAIL}"
-    zfs create -o mountpoint=${MP_JAIL} ${DS_JAIL}
-else
-    echo "  already present: ${DS_JAIL}"
-fi
-if [ ! "`zfs list | egrep ${TEMPLATE_DS}`" ]; then
-    echo "  zfs create: ${TEMPLATE_DS}"
-    zfs create -p ${TEMPLATE_DS}
-else
-    echo "  already present: ${TEMPLATE_DS}"
-fi
-
-if [ ! "`zfs list | egrep ${SKELETON_DS}`" ]; then
-    echo "  zfs create: ${SKELETON_DS}"
-    zfs create -p ${SKELETON_DS}
-else
-    echo "  already present: ${SKELETON_DS}"
-fi
-
-echo "Creating RELEASE snapshot"
-if [ ! -e ${SENTINEL_REL_DONE} ]; then
-
-    if [ ! "`zfs list | egrep ${RELEASE_DS}`" ]; then
-        echo "  zfs create: ${RELEASE_DS}"
-        zfs create -p ${RELEASE_DS}
-        mkdir -p ${SENTINEL_DIR}
+zfs_optionally_create() {
+    if [ ! "`zfs list | egrep ${1}`" ]; then
+        echo "  zfs create: $1"
+        zfs create -p $1
+        if [ $2 ]; then
+            zfs set mountpoint=$2 $1
+        fi
+    else
+        echo "  already present: ${1}"
     fi
+}
 
+zfs_mountpoint() {
+    echo "`zfs get mountpoint $1 | tail -1 | awk '{print $3}'`"
+}
 
-    if [ ! -e ${SENTINEL_FIRST} ]; then
-        echo "  template container components: fetch"
+create_layer_0_zfs(){
+    echo "Creating ZFS layer"
+    zfs_optionally_create ${DS_JAIL} ${MP_JAIL}
+    zfs_optionally_create ${RELEASE_DS}
+    zfs_optionally_create ${BASE_DS}
+    zfs_optionally_create ${SKELETON_DS}
+    zfs_optionally_create ${THINJAIL_DS}
+}
+
+create_layer_1_release(){
+
+    echo "Creating RELEASE snapshot for ${OS_JAIL}"
+
+    RELEASE_MP=`zfs_mountpoint ${RELEASE_DS}`
+    RELEASE_SNAP=${RELEASE_DS}@${DATE}
+    SENTINEL_L1_DIR=${RELEASE_MP}/sentinel
+    SENTINEL_L1_DONE=${SENTINEL_L1_DIR}/layer-1
+
+    if [ ! -e ${SENTINEL_L1_DONE} ]; then
+        mkdir -p ${SENTINEL_L1_DIR}
+
         RELTMP=`mktemp -d "/tmp/jailbootstrap.XXXXXX"`
         trap 'rm -r "$RELTMP"' EXIT
 
-        fetch ftp://ftp.freebsd.org/pub/FreeBSD/releases/amd64/amd64/${OS_JAIL}/base.txz -o  ${RELTMP}/base.txz
-        fetch ftp://ftp.freebsd.org/pub/FreeBSD/releases/amd64/amd64/${OS_JAIL}/lib32.txz -o ${RELTMP}/lib32.txz
+        echo "  Fetching components"
+        fetch https://ftp.freebsd.org/pub/FreeBSD/releases/amd64/amd64/${OS_JAIL}/base.txz -qo  ${RELTMP}/base.txz
+        fetch https://ftp.freebsd.org/pub/FreeBSD/releases/amd64/amd64/${OS_JAIL}/lib32.txz -qo ${RELTMP}/lib32.txz
 
+        echo "  Extracting components"
         tar -xf ${RELTMP}/base.txz  -C ${RELEASE_MP}
         tar -xf ${RELTMP}/lib32.txz -C ${RELEASE_MP}
 
-        touch ${SENTINEL_FIRST}
+
+        touch ${SENTINEL_L1_DONE}
+
+        echo "  Creating snapshot [${RELEASE_SNAP}]"
+        zfs snapshot ${RELEASE_SNAP}
     else
-        echo "  template container components already present"
+        echo "  ${OS_JAIL} snapshot already present"
     fi
+}
 
-    if [ ! -e ${SENTINEL_UPD_FETCH} ]; then
-        echo "  update template container components: fetch"
-        env UNAME_r=${OS_JAIL} freebsd-update --not-running-from-cron -b ${RELEASE_MP} fetch
-        touch ${SENTINEL_UPD_FETCH}
+create_layer_2_base(){
+
+    echo "Creating BASE (updatable) snapshot from RELEASE"
+
+    BASE_MP=`zfs_mountpoint ${BASE_DS}`
+
+    RELEASE_MP=`zfs_mountpoint ${RELEASE_DS}`
+    RELEASE_SNAP_LAST=`zfs list -rt snapshot | egrep RELEASE | tail -1 | awk '{print $1}'`
+
+    SENTINEL_L2_DIR=${BASE_MP}/sentinel
+    SENTINEL_L2_DONE=${SENTINEL_L2_DIR}/layer-2
+
+    if [ ! -e ${SENTINEL_L2_DONE} ]; then
+        if [ "`zfs list | egrep ${BASE_DS}`" ]; then
+            echo "  Stale BASE detected, destroying"
+            zfs destroy -f ${BASE_DS}
+        fi
+
+        echo "  Cloning RELEASE to BASE"
+        echo "zfs clone -p ${RELEASE_SNAP_LAST} ${BASE_DS}"
+        zfs clone -p ${RELEASE_SNAP_LAST} ${BASE_DS}
+
+        cp /etc/localtime ${BASE_MP}/etc/localtime
+
+        echo "  Executing freebsd-update fetch in [${BASE_MP}]"
+        env UNAME_r=${OS_JAIL} freebsd-update --not-running-from-cron -b ${BASE_MP} fetch
+
+        if [ -e /var/db/freebsd-update/`echo ${BASE_MP} | sha256`-install ]; then
+            echo "  Executing freebsd-update install in [${BASE_MP}]"
+            freebsd-update -b ${BASE_MP} install
+        else
+            echo "  Detected no need to install updates, proceeding"
+        fi
+
+        touch ${SENTINEL_L2_DONE}
     else
-        echo "  container component updates already fetched"
+        echo "  No need to create base image"
     fi
+}
 
-    # Check freebsd-update director for
-    if [ -e /var/db/freebsd-update/`echo ${RELEASE_MP} | sha256`-install ]; then
-        echo "  update template container components: install"
-        freebsd-update -b ${RELEASE_MP} install
+create_layer_3_skeleton(){
+
+    echo "Creating SKELETON dataset from BASE"
+
+    BASE_MP=`zfs_mountpoint ${BASE_DS}`
+
+    SKELETON_MP=`zfs_mountpoint ${SKELETON_DS}`
+    SKELETON_SNAP=${SKELETON_DS}@skeleton
+
+    SENTINEL_L3_DIR=${BASE_MP}/sentinel
+    SENTINEL_L3_DONE=${SENTINEL_L3_DIR}/layer-3
+
+    if [ ! -e ${SENTINEL_L3_DONE} ]; then
+        cd ${BASE_MP}
+        echo "  Copying skeleton files from base template"
+        mkdir -p ${SKELETON_MP}/home
+        mkdir -p ${SKELETON_MP}/usr
+        chflags 0 ${BASE_MP}/var/empty
+        rm -rf ${BASE_MP}/var/empty
+        mv ${BASE_MP}/etc       ${SKELETON_MP}/etc
+        mv ${BASE_MP}/usr/local ${SKELETON_MP}/usr/local
+        mv ${BASE_MP}/tmp       ${SKELETON_MP}/tmp
+        mv ${BASE_MP}/var       ${SKELETON_MP}/var
+        mv ${BASE_MP}/root      ${SKELETON_MP}/root
+
+        echo "  Setting up links to skeleton files"
+        mkdir skeleton
+        ln -s skeleton/etc etc
+        ln -s skeleton/home home
+        ln -s skeleton/root root
+        ln -s ../skeleton/usr/local usr/local
+        ln -s skeleton/tmp tmp
+        ln -s skeleton/var var
+
+        echo "  Creating skeleton template snapshot"
+        zfs snapshot ${SKELETON_SNAP}
+
+        touch ${SENTINEL_L3_DONE}
     else
-        echo "  template container component updates already installed"
+        echo "  No need to create skeleton data"
     fi
+}
 
-    cp /etc/localtime ${RELEASE_MP}/etc/localtime
-    zfs snapshot ${RELEASE_SNAP}
-
-    echo "Creating base template"
-    LAST_REL_SNAP=`zfs list -rt snapshot | egrep RELEASE | tail -1 | awk '{print $1}'`
-    if [ "`zfs list | egrep ${BASE_DS}`" ]; then
-        echo "  old base template detected, destroying"
-        zfs destroy -f ${BASE_DS}
-    fi
-    zfs clone -p ${LAST_REL_SNAP} ${BASE_DS}
-
-    cd ${BASE_MP}
-    echo "Initalizing skeleton template from base template"
-    mkdir -p ${SKELETON_MP}/home
-    mkdir -p ${SKELETON_MP}/usr
-    chflags 0 ${BASE_MP}/var/empty
-    rm -rf ${BASE_MP}/var/empty
-    mv ${BASE_MP}/etc       ${SKELETON_MP}/etc
-    mv ${BASE_MP}/usr/local ${SKELETON_MP}/usr/local
-    mv ${BASE_MP}/tmp       ${SKELETON_MP}/tmp
-    mv ${BASE_MP}/var       ${SKELETON_MP}/var
-    mv ${BASE_MP}/root      ${SKELETON_MP}/root
-
-    echo "Setting up skeleton template"
-    mkdir skeleton
-    ln -s skeleton/etc etc
-    ln -s skeleton/home home
-    ln -s skeleton/root root
-    ln -s ../skeleton/usr/local usr/local
-    ln -s skeleton/tmp tmp
-    ln -s skeleton/var var
-
-    echo "Creating skeleton template snapshot"
-    zfs snapshot ${SKELETON_DS}@skeleton
-
-    # Set release-done sentinel
-    touch ${SENTINEL_REL_DONE}
-else
-    echo "  RELEASE snapshot detected at ${RELEASE_MP}, nothing to do"
-fi"""}
+create_layer_0_zfs
+create_layer_1_release
+create_layer_2_base
+create_layer_3_skeleton
